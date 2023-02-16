4a11a5da0641d9cdca18317ae9ab4af70ec41c8e
Randolph <cliu603@gmail.com>
2023-02-16 20:23:12 +1300

    welcome words

--- src/components/roomFunction.ts
+++ src/components/roomFunction.ts
@@ -12,9 +12,9 @@ const welcomeNewMember = async (bot: Wechaty, msg: Message): Promise<void> => {
         console.log('newMember: ' + JSON.stringify(newMembers));
         // https://emojipedia.org/wechat/
         const content = `[Party]欢迎新人 ${newMemberName}，本群旨在认识新朋友[LetMeSee]，锻炼身体[GoForIt]，提高羽毛球水平[Yeah!]。\n`
-            + `群里羽毛球活动目前暂水平无差别活动，希望参加的成员彼此照顾。使用群内程序报名。\n`
-            + `疫情期间，请群友们不打球在场下尽量带好口罩[KeepFighting]，参加活动请带好疫苗证书。\n`
-            + `请不要在群里乱发广告，可以乱发红包[Packet]，请勿直接添加群成员，我们是有爱的小集体!`;
+            + `群里羽毛球活动目前暂水平无差别活动，希望参加的成员彼此照顾。使用群内程序报名。详情请查看群公告。\n`
+            + `疫情期间，请群友们不打球在场下尽量带好口罩[KeepFighting]。\n`
+            + `请不要在群里乱发广告，可以乱发红包[Packet]，请勿直接添加群成员，我们是有爱的小集体!`;         
 
         if (newMembers.length > 0) {
             await room?.say(content, ...newMembers);
