c5a383d08644ec3104504b9e60c78af417af39d4
Randolph <cliu603@gmail.com>
2023-08-28 10:02:24 +1200

    tidy up

--- cloud-bot/components/furtuneTelling.ts
+++ cloud-bot/components/furtuneTelling.ts
@@ -2,7 +2,7 @@ import { Contact, Room } from 'wechaty';
 import items from '../configs/fortuneTelling.json' assert { type: "json" };
 import { FortuneTelling } from '../model/fortuneTelling';
 
-const historyCache: { id: string, date: string, fortuneTelling: FortuneTelling }[] = [];
+const historyCache: { talkerId: string, date: string, fortuneTelling: FortuneTelling, index: number }[] = [];
 const itemList = items as FortuneTelling[];
 
 
@@ -10,26 +10,25 @@ const tellMeFortune = async (room: Room, talker: Contact): Promise<void> => {
     const talkerId = talker.id
     if (talkerId) {
         const today = new Date().toLocaleDateString();
-        const existingTodayIndex = historyCache.findIndex(h => h.id === talkerId && h.date === today);
-        if (existingTodayIndex > 0) {
-            const existingToday = historyCache.at(existingTodayIndex);            
-            const content = `您抽到了第${existingTodayIndex}签!\n`
+        const existingToday = historyCache.find(h => h.talkerId === talkerId && h.date === today);
+        if (existingToday) {            
+            const content = `您抽到了第${existingToday.index}签!\n`
                 + '-----------------\n'
-                + `🎐签诗：${existingToday?.fortuneTelling.signature}\n`
+                + `🎐签诗：${existingToday.fortuneTelling.signature}\n`
                 + '-----------------\n'
-                + '需要解签请回复【小白云 解签】';
-            await room.say(content, talker);            
+                + '需要解签请回复【小白云 解签】\n';
+            await room.say(content.trim(), talker);
         } else {
             const rndInt = Math.floor(Math.random() * itemList.length) + 1;
             const fortuneTelling = itemList[rndInt];
-            historyCache.push({ id: talkerId, date: today, fortuneTelling });
+            historyCache.push({ talkerId, date: today, fortuneTelling, index: rndInt });
 
             const content = `您抽到了第${rndInt}签!\n`
                 + '-----------------\n'
                 + `🎐签诗：${fortuneTelling.signature}\n`
                 + '-----------------\n'
-                + '需要解签请回复【小白云 解签】';
-            await room.say(content, talker);
+                + '需要解签请回复【小白云 解签】\n';
+            await room.say(content.trim(), talker);
         }
     }
 }
@@ -38,12 +37,14 @@ const explainWhy = async (room: Room, talker: Contact): Promise<void> => {
     const talkerId = talker.id
     if (talkerId) {
         const today = new Date().toLocaleDateString();
-        const existingToday = historyCache.find(h => h.id === talkerId && h.date === today);
+        const existingToday = historyCache.find(h => h.talkerId === talkerId && h.date === today);
         if (existingToday) {
-            const content = ''
-                + '\n-----------------\n'
-                + `🎐解签：${existingToday.fortuneTelling.untick2}`;
-            await room.say(content, talker);
+            const content = '🎐解签：\n'
+                + '-----------------\n'
+                + `${existingToday.fortuneTelling.untick1}\n`
+                + '-----------------\n'
+                + `${existingToday.fortuneTelling.untick2}`;
+            await room.say(content.trim(), talker);
         } else {
             await room.say("请先抽签", talker);
         }
